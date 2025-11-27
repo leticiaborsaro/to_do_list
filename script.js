8e932c37330e02ea1e115a835b38240add68c650
Letícia Borsaro <leleborsaro@gmail.com>
2025-11-27 16:22:25 -0300

    mudei icone lixeira

--- script.js
+++ script.js
@@ -116,7 +116,10 @@ document.addEventListener('DOMContentLoaded', () => {
             const button = document.createElement('button');
             button.className = 'delete-projeto-btn';
             button.title = `Apagar projeto "${project.name}"`;
-            button.innerHTML = `<svg class="icon-small" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0l.346-9m3.54 0l-1.096-1.096a.75.75 0 00-1.06 0L12 7.904l-1.096-.096a.75.75 0 00-1.06 0l-1.096 1.096M14.74 9H9.26m-4.813 0h14.106c.338 0 .612.274.612.612v.016c0 .338-.274.612-.612.612H4.427a.612.612 0 01-.612-.612v-.016c0-.338.274-.612.612-.612zM4.427 9v10.188c0 .338.274.612.612.612h14.106c.338 0 .612-.274.612-.612V9.016" /></svg>`;
+            button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>`;
             
             button.addEventListener('click', (e) => {
                 e.stopPropagation();
